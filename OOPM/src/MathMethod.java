da090284f6e99d0148f291b7af08e18ea1fee4f4
HarshalKondke <harshalkondke2014@gmail.com>
2019-07-16 23:16:17 +0530

    examples of different methods under math fn...

---
+++ OOPM/src/MathMethod.java
@@ -0,0 +1,12 @@
+class MathMethods{
+	public static void main(String args[]){
+		System.out.println("\033[H\033[2J");
+		System.out.println("Math.E " + Math.E);
+		System.out.println("Math.PI "+Math.PI);
+		System.out.println("Math.abs(-1234) "+Math.abs(-1234));
+		System.out.println("Math.cos(Math.PI/4) "+Math.cos(Math.PI/4));
+		System.out.println("Math.sin(Math.PI/2) "+Math.sin(Math.PI/2));
+		System.out.println("Math.log(1) "+Math.log(1));
+		System.out.println("Math.exp(Math.PI) "+Math.exp(Math.PI));
+	}
+}
