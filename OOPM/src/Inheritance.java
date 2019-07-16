87053ca58f1ee0c3a69d4d11be04f9d46ca75a13
HarshalKondke <harshalkondke2014@gmail.com>
2019-07-16 23:16:35 +0530

    revised.

--- OOPM/src/Inheritance.java
+++ OOPM/src/Inheritance.java
@@ -1,4 +1,32 @@
+//base class or parent class
+class Bicycle{
+	public int gear;
+	public int speed;
+	
+	public Bicycle(int gear,int speed){
+		this.gear=gear;
+		this.speed=speed;
+	}
+	public String display(){
+		return ("No of gears are : "+gear+"\n"+"Speed of Bicycle : "+speed);
+	}
+}
 
-public class Inheritance {
+class MountainBike extends Bicycle{
+	public int height;
+	
+	public MountainBike(int gear,int speed,int height){
+		super(gear, speed); // in-vooooooking base class constructor
+		this.height=height;
+	}
+	public String display(){
+		return(super.display()+"\n"+"Height is : "+height);
+	}
+}
 
+class Inheritance{
+	public static void main(String args[]){
+		MountainBike muk = new MountainBike(3,45,130);
+		System.out.println(muk.display());
+	}
 }
